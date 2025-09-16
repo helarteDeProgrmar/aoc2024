9558d1b556419ab4836a2c072b21d53d199fadae
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-09-16 15:05:48 +0200

    feat:complete the first part before go with fati

--- lib/day9/day9.ex
+++ lib/day9/day9.ex
@@ -1,6 +1,13 @@
 defmodule Day9 do
   def main() do
-    IO.puts("Hello!")
-    6
+    File.read!("lib/day9/input")
+    |> IO.inspect()
+    |> String.graphemes()
+    |> IO.inspect()
+    |> Enum.map(fn l -> String.to_integer(l) end)
+  end
+
+  def transform_integer_representation_to_memory(integers) do
+    nil
   end
 end
